252cec542c55b7484d895e032569433b70b4d3e8
João Luiz Lagreca <joaoluizlagrecadasilva@gmail.com>
2021-10-31 15:14:24 -0300

    Create Ordenar.java

---
+++ src/br/senac/grupo10/util/Ordenar.java
@@ -0,0 +1,20 @@
+package br.senac.grupo10.util;
+
+public class Ordenar {
+	
+	/**Trocas*/
+	public static void bubbleSort() {
+		//TODO: Ordenação por trocas
+	}
+	
+	/**Seleção*/
+	public static void selectionSort() {
+		//TODO: Ordenação por seleção
+	}
+	
+	/**Inserção*/
+	public static void insertionSort() {
+		//TODO: Ordenação por inserção
+	}
+
+}
