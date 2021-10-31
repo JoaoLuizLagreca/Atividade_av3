b0bb97b1b80c31a46d644226bc31cc9f8bd8281f
João Luiz Lagreca <joaoluizlagrecadasilva@gmail.com>
2021-10-31 15:14:21 -0300

    Criação da classe Pilha e inserção de dados

--- src/br/senac/grupo10/Main.java
+++ src/br/senac/grupo10/Main.java
@@ -1,6 +1,43 @@
 package br.senac.grupo10;
+
+import br.senac.grupo10.util.Pilha;
+
 public class Main {
+	
+	static class Aluno{
+		Aluno(float nota, String nome){
+			this.nota=nota;
+			this.nome=nome;
+		}
+		float nota;
+		String nome;
+	}
+	
+	
+	private static Pilha<Aluno> alunos=new Pilha<Aluno>(18);
+	static { //FIXME: Testar valores
+		alunos.push(new Aluno(7.0f, "Carlos"));
+		alunos.push(new Aluno(7.0f, "Rosana"));
+		alunos.push(new Aluno(7.5f, "Diego"));
+		alunos.push(new Aluno(7.5f, "Nair"));
+		alunos.push(new Aluno(7.5f, "Paula"));
+		alunos.push(new Aluno(8.0f, "Fernando"));
+		alunos.push(new Aluno(8.0f, "Flavio"));
+		alunos.push(new Aluno(8.0f, "Laura"));
+		alunos.push(new Aluno(8.5f, "Denise"));
+		alunos.push(new Aluno(8.5f, "Jose"));
+		alunos.push(new Aluno(8.5f, "Julio"));
+		alunos.push(new Aluno(9.5f, "Claudio"));
+		alunos.push(new Aluno(9.5f, "Elisa"));
+		alunos.push(new Aluno(9.5f, "Elisangela"));
+		alunos.push(new Aluno(9.5f, "Fabricio"));
+		alunos.push(new Aluno(9.5f, "Rafael"));
+		alunos.push(new Aluno(9.5f, "Teodoro"));
+		alunos.push(new Aluno(9.5f, "Valeria"));
+	}
+	
 	public static void main(String args[]) {
 		
 	}
+
 }
---
+++ src/br/senac/grupo10/util/Pilha.java
@@ -0,0 +1,53 @@
+package br.senac.grupo10.util;
+
+//FIXME: testar pilha
+public class Pilha <Tipo>{
+
+	private Object[] vetor;
+	private int pos=-1;
+	
+	public Pilha(int capacidade) {
+		if(capacidade<=0) throw new IllegalArgumentException("Capacidade deve ser maior que 0");
+		this.vetor = new Object[capacidade];
+	}
+	
+	/**Adiciona um valor no topo da pilha*/
+	public void push(Tipo valor) {
+		if(pos>=vetor.length-1) throw new IndexOutOfBoundsException("Estouro da pilha");
+		pos++;
+		vetor[pos]=valor;
+	}
+	
+	/**Retorna o valor do topo da pilha e o remove*/
+	@SuppressWarnings("unchecked")
+	public Tipo pop() {
+		if(pos<0) throw new IndexOutOfBoundsException("Pilha está vazia");
+		pos--;
+		return (Tipo)vetor[pos+1];
+	}
+	
+	/**Retorna o valor do topo da pilha*/
+	@SuppressWarnings("unchecked")
+	public Tipo peek() {
+		if(pos<0) throw new IndexOutOfBoundsException("Pilha está vazia");
+		return (Tipo)vetor[pos];
+	}
+	
+	/**Retorna se a pilha está vazia*/
+	public boolean empty() {
+		return pos<0;
+	}
+	
+	/**Retorna o valor do índice i da pilha*/
+	@SuppressWarnings("unchecked")
+	public Tipo get(int i) {
+		if(i>pos) throw new IndexOutOfBoundsException();
+		return (Tipo) vetor[i];
+	}
+	
+	/**Retorna a capacidade da pilha*/
+	public int capacity() {
+		return vetor.length;
+	}
+	
+}
