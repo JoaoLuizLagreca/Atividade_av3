ce2be72ef1d49549f840be373da723a5487f643d
João Luiz Lagreca <joaoluizlagrecadasilva@gmail.com>
2021-11-01 20:54:34 -0300

    Create Pesquisar.java

---
+++ src/br/senac/grupo10/util/Pesquisar.java
@@ -0,0 +1,17 @@
+package br.senac.grupo10.util;
+
+public class Pesquisar {
+	
+	/**Pesquisa sequencial*/
+	public Aluno sequenceSearch(Fila<Aluno> alunos) {
+		throw new NullPointerException("Essa função é um toco e não foi implementada ainda");
+		//TODO: Implementar função de pesquisa sequencial
+	}
+	
+	/**Pesquisa binária*/
+	public Aluno binarySearch(Fila<Aluno> alunos) {
+		throw new NullPointerException("Essa função é um toco e não foi implementada ainda");
+		//TODO: Implementar função de pesquisa binária
+	}
+
+}
