fac21243e5391023b31f7817e10c3a2591656cbf
João Luiz Lagreca <joaoluizlagrecadasilva@gmail.com>
2021-11-01 06:28:45 -0300

    Reestruturação de dados

--- src/br/senac/grupo10/Main.java
+++ src/br/senac/grupo10/Main.java
@@ -1,48 +1,57 @@
 package br.senac.grupo10;
 
 import br.senac.grupo10.util.Aluno;
+import br.senac.grupo10.util.Fila;
 import br.senac.grupo10.util.Pilha;
 
 public class Main {
 	
+	private static float NOTA_MINIMA=5.0f;
 	
-	private static Pilha<Aluno> alunos=new Pilha<Aluno>(30);
+	private static Fila<Aluno> alunos = new Fila<Aluno>();
 	static { //FIXME: Testar valores
 		
-		alunos.push(new Aluno(7.0f, "Rosana"));
-		alunos.push(new Aluno(8.5f, "Denise"));
-		alunos.push(new Aluno(6.0f, "Jean"));
-		alunos.push(new Aluno(5.5f, "Felipe"));
-		alunos.push(new Aluno(9.5f, "Teodoro"));
-		alunos.push(new Aluno(6.5f, "Mateus"));
-		alunos.push(new Aluno(6.0f, "Lucas"));
-		alunos.push(new Aluno(9.5f, "Claudio"));
-		alunos.push(new Aluno(8.0f, "Fernando"));
-		alunos.push(new Aluno(6.0f, "Jonas"));
-		alunos.push(new Aluno(9.5f, "Elisa"));
-		alunos.push(new Aluno(7.5f, "Paula"));
-		alunos.push(new Aluno(7.5f, "Diego"));
-		alunos.push(new Aluno(9.5f, "Valeria"));
-		alunos.push(new Aluno(8.0f, "Laura"));
-		alunos.push(new Aluno(5.0f, "Emerson"));
-		alunos.push(new Aluno(6.0f, "Everton"));
-		alunos.push(new Aluno(8.5f, "Jose"));
-		alunos.push(new Aluno(8.5f, "Julio"));
-		alunos.push(new Aluno(9.5f, "Fabricio"));
-		alunos.push(new Aluno(8.0f, "Flavio"));
-		alunos.push(new Aluno(6.0f, "Pamela"));
-		alunos.push(new Aluno(6.5f, "Neide"));
-		alunos.push(new Aluno(5.0f, "Marcos"));
-		alunos.push(new Aluno(6.5f, "Nilton"));
-		alunos.push(new Aluno(7.0f, "Carlos"));
-		alunos.push(new Aluno(9.5f, "Rafael"));
-		alunos.push(new Aluno(5.5f, "Mariana"));
-		alunos.push(new Aluno(7.5f, "Nair"));
-		alunos.push(new Aluno(9.5f, "Elisangela"));
+		alunos.add(new Aluno(9.5f, "Elisangela"));
+		alunos.add(new Aluno(7.5f, "Nair"));
+		alunos.add(new Aluno(5.5f, "Mariana"));
+		alunos.add(new Aluno(9.5f, "Rafael"));
+		alunos.add(new Aluno(7.0f, "Carlos"));
+		alunos.add(new Aluno(6.5f, "Nilton"));
+		alunos.add(new Aluno(5.0f, "Marcos"));
+		alunos.add(new Aluno(6.5f, "Neide"));
+		alunos.add(new Aluno(6.0f, "Pamela"));
+		alunos.add(new Aluno(8.0f, "Flavio"));
+		alunos.add(new Aluno(9.5f, "Fabricio"));
+		alunos.add(new Aluno(8.5f, "Julio"));
+		alunos.add(new Aluno(8.5f, "Jose"));
+		alunos.add(new Aluno(6.0f, "Everton"));
+		alunos.add(new Aluno(5.0f, "Emerson"));
+		alunos.add(new Aluno(8.0f, "Laura"));
+		alunos.add(new Aluno(9.5f, "Valeria"));
+		alunos.add(new Aluno(7.5f, "Diego"));
+		alunos.add(new Aluno(7.5f, "Paula"));
+		alunos.add(new Aluno(9.5f, "Elisa"));
+		alunos.add(new Aluno(6.0f, "Jonas"));
+		alunos.add(new Aluno(8.0f, "Fernando"));
+		alunos.add(new Aluno(9.5f, "Claudio"));
+		alunos.add(new Aluno(6.0f, "Lucas"));
+		alunos.add(new Aluno(6.5f, "Mateus"));
+		alunos.add(new Aluno(9.5f, "Teodoro"));
+		alunos.add(new Aluno(5.5f, "Felipe"));
+		alunos.add(new Aluno(6.0f, "Jean"));
+		alunos.add(new Aluno(8.5f, "Denise"));
+		alunos.add(new Aluno(7.0f, "Rosana"));
+	
 		
 	}
 	
 	public static void main(String args[]) {
+		
+		for(int i=0; i<alunos.size(); i++) {
+			Aluno a=alunos.get(i);
+			System.out.println(a.nota+" "+a.nome);
+		}
+		System.out.println();
 		
 	}
 
