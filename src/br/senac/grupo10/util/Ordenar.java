968f2cb382716527e644ab59b4c0b30da490455c
João Luiz Lagreca <joaoluizlagrecadasilva@gmail.com>
2021-11-01 17:38:17 -0300

    Ordenação por Bubble Sort implementado

--- src/br/senac/grupo10/util/Ordenar.java
+++ src/br/senac/grupo10/util/Ordenar.java
@@ -1,19 +1,39 @@
 package br.senac.grupo10.util;
 
+import br.senac.grupo10.util.Fila.nó;
+
 public class Ordenar {
 	
 	/**Trocas*/
-	public static void bubbleSort() {
-		//TODO: Ordenação por trocas
+	public static void bubbleSort(Fila<Aluno> alunos) {
+		nó<Aluno> a;
+		Aluno al;
+		boolean h;
+		for(int i=0; i<alunos.size()-1; i++) {
+			h=false;
+			alunos.get(0);
+			for(int j=0; j<alunos.size()-1-i; j++) {
+				a = alunos.getNo();
+				alunos.next();
+				if(a.valor.nota<alunos.get().nota) { //Fazer a troca se o próximo valor for maior
+					al=a.valor;
+					a.valor=alunos.get();
+					alunos.getNo().valor=al;
+					h=true;
+				}
+			}
+			if(!h)
+				return; //Parar imediatamente se não houver a troca
+		}
 	}
 	
 	/**Seleção*/
-	public static void selectionSort() {
+	public static void selectionSort(String a[]) {
 		//TODO: Ordenação por seleção
 	}
 	
 	/**Inserção*/
-	public static void insertionSort() {
+	public static void insertionSort(String a[]) {
 		//TODO: Ordenação por inserção
 	}
 
